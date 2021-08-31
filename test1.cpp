298b8a6765c5e70f4158de5c6079b99243754e56
Alejandra De Luna <88346584+profaledelunap@users.noreply.github.com>
2021-08-31 12:24:16 -0500

    descending order

--- test1.cpp
+++ test1.cpp
@@ -17,7 +17,8 @@ using namespace std;
 
 TEST_CASE("Testing descendingSort: Test 1", "[descendingSort]")
 {
-	vector<int> vec1in{8, 10, 4, 8, 12, 20, 15, 54, 18}, vec1out{4, 8, 8, 10, 12, 15, 18, 20, 54};
+
+	vector<int> vec1in{8, 10, 4, 8, 12, 20, 15, 54, 18}, vec1out{54, 20, 18, 15, 12, 10, 8, 8, 4};
 
 	REQUIRE(descendingSort(vec1in) == vec1out);
 }
